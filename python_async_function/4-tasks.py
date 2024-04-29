5aebc46b86b3ff1e51369cac53ff74f48b43d6c6
aytachuseynli <aytac_ha@mail.ru>
2024-04-29 15:04:06 +0400

    ADDED

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module: 4-tasks
+Contains:
+    - task_wait_n: Returns a list of asyncio.Tasks
+"""
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a list of asyncio.Tasks."""
+    return await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
