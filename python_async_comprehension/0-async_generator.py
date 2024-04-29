9ada36cc87b6be74c9c05b47e43f932fafd268a2
aytachuseynli <aytac_ha@mail.ru>
2024-04-29 15:15:07 +0400

    Added

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -12,7 +12,9 @@ import random
 
 
 async def async_generator() -> float:
-    """Asynchronous generator that yields random numbers."""
+    """
+    Asynchronous generator that yields random numbers.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
