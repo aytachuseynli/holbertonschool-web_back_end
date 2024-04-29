9030e6302c514e2660595f9f98f7c2611f652c4b
aytachuseynli <aytac_ha@mail.ru>
2024-04-29 13:12:12 +0400

    initial commit

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,7 +9,9 @@ Contains:
 
 from typing import Union, Tuple
 
-
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return a tuple containing a string k and the square of an int or float v."""
+    """
+    Return a tuple containing a string k and
+    the square of an int or float v.
+    """
     return (k, v ** 2)
