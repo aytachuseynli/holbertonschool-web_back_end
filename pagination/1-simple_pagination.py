065dd40eba16169df241f68a2a67aba126694247
aytachuseynli <aytac_ha@mail.ru>
2024-05-11 15:38:37 +0400

    Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Module for simple pagination with a Server class.
+"""
+import csv
+from typing import List, Tuple
+from itertools import islice
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of start and end indexes corresponding to the range
+    of indexes to return in a list for a particular pagination.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0, "page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        return list(islice(dataset, start_index, end_index))
