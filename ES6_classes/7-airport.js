8e63428af314e86e0542e46551c3bd842ffd0b50
aytachuseynli <aytac_ha@mail.ru>
2024-05-19 01:50:31 +0400

    update

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,12 +1,8 @@
-/*7-airport.js*/
-
 export default class Airport {
   constructor(name, code) {
-      this._name = name;
-      this._code = code;
+    this._name = name;
+    this._code = code;
   }
 
-  get [Symbol.toStringTag]() {
-      return `${this._code}`;
-  }
+  toString() { return `[object ${this._code}]`; }
 }
