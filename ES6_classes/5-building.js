89128f17625dc01d6d3c0f3debaef406f2a56fc0
aytachuseynli <aytac_ha@mail.ru>
2024-05-19 01:49:36 +0400

    update

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,13 @@
-/*5-building.js*/
-
+/* Define the Building class */
 export default class Building {
   constructor(sqft) {
-      if (this.constructor !== Building && !this.evacuationWarningMessage) {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-      this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
   }
 
   get sqft() {
-      return this._sqft;
+    return this._sqft;
   }
 }
