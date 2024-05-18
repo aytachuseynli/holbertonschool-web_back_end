f431241003b5b3a47df22b87022a0d424d04cb83
aytachuseynli <aytac_ha@mail.ru>
2024-05-19 01:45:40 +0400

    update

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,47 +1,26 @@
-/*Define a class Currency*/
-/* eslint-disable */
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-      if (typeof name !== 'string') throw TypeError('Name must be a string'); {
-          this._name = name;
-      }
-      if (typeof length !== 'number') throw TypeError('Length must be a number'); {
-          this._length = length;
-      }
-      if (!Array.isArray(students)) throw TypeError('Students must be an array'); {
-          this._students = students;
-      }
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
   }
 
   get name() {
-      return this._name;
+    return this._name;
   }
 
-  set name(newName) {
-      if (typeof newName !== 'string') throw TypeError('Name must be a string'); {
-          this._name = newName;
-      }
+  set code(code) {
+    this._code = code;
   }
 
-
-  get length() {
-      return this._length;
+  set name(name) {
+    this._name = name;
   }
 
-  set length(newLength) {
-      if (typeof newLength !== 'number') throw TypeError('Length must be a number'); {
-          this._length = newLength;
-      }
-  }
-
-
-  get students() {
-      return this._students;
-  }
-
-  set students(newStudents) {
-      if (!Array.isArray(newStudents)) throw TypeError('Students must be an array'); {
-          this._students = newStudents;
-      }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,59 +1,30 @@
-/*4-pricing.js*/
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-import Currency from './3-currency.js';
+  get amount() {
+    return this._amount;
+  }
 
+  get currency() {
+    return this._currency;
+  }
 
-class Pricing {
-    constructor (amount, currency) {
-        if (typeof amount === 'number'
-        && currency instanceof Currency) {
-            this._amount = amount;
-            this._currency = currency;
-        }
-        else {
-            throw TypeError('Invalid attribute type');
-        }
-    }
+  set amount(amount) {
+    this._amount = amount;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  set currency(currency) {
+    this._currency = currency;
+  }
 
-    set amount(newAmount) {
-        if (typeof newAmount === 'number') {
-            this._amount = newAmount;
-        }
-        else {
-            throw TypeError('Amount must be a number');
-        }
-    }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    get currency() {
-        return this._currency;
-    }
-
-    set currency(newCurrency) {
-        if (newCurrency instanceof Currency) {
-            this._currency = newCurrency;
-        }
-        else {
-            throw TypeError('Currency must be a Currency');
-        }
-    }
-
-    displayFullPrice() {
-        return `${this.amount} ${this.currency.displayFullCurrency()}`;
-    }
-
-    static convertPrice(amount, conversionRate) {
-        if (typeof amount === 'number'
-        && typeof conversionRate === 'number') {
-            return new Pricing(amount * conversionRate, this.currency);
-        }
-        else {
-            throw TypeError('Invalid attribute type');
-        }
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
-
-export default Pricing;
