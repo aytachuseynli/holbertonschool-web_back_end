c8eda4ad75ffb570f8e43ee9c1741bfd65e5a111
aytachuseynli <aytac_ha@mail.ru>
2024-05-18 21:23:12 +0400

    Added ClassRoom

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,8 @@
+// 0-classroom.js
+
 export default class ClassRoom {
-    contructor(maxStudentSize) {
-        this._maxStudentSize = maxStudentSize;
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
     }
-}
+  }
+  
--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line import/extensions
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-    return [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ];
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
 }
+
+export default initializeRooms;
